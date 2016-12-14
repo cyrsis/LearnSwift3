53f13422ff91821d87ad926edd5f30e8443327b9
Victor <em3888@gmail.com>
2016-12-14 15:22:24 +0800

    Add Optional or Type checking

--- Swift3Playground.playground/Contents.swift
+++ Swift3Playground.playground/Contents.swift
@@ -1,7 +1,30 @@
-//: Playground - noun: a place where people can play
 
 import UIKit
-//Lessons
+
+//*** Lessons Optional
+
+//Optional -> What is optional?
+//String to Int
+
+var str = "Hello, playground"
+
+//let strNumber = "123"
+let strNumber = "ABC"
+
+func convertsStringInterger(str:String)->Int?{ //Can be a global function
+    
+    return Int(str)
+}
+if let i = convertsStringInterger(str:strNumber){
+    i.bigEndian
+}else{
+    // Check Types conversations
+    // Try let strNumber to N123s
+    print("number could not be turned into an integer")
+}
+
+
+//*** Lessons Class and Objects
 
 
 class Person {
